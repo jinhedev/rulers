90b7918f8bd8739b2d9794a73d1aea9f0b17e3fe
sudofluff <sudofluff@icloud.com>
2019-06-30 18:34:07 -0700

    Removes edge case handler for root request

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -3,7 +3,6 @@ require "rulers/routing"
 require "rulers/util"
 require "rulers/dependencies"
 require "rulers/controller"
-
 require "rulers/file_model"
 
 module Rulers
@@ -13,20 +12,6 @@ module Rulers
       if env['PATH_INFO'] == '/favicon.ico'
         return [404, {'Content-Type' => 'text/html'}, []]
       end
-
-      # if env['PATH_INFO'] == '/'
-      #   byebug
-      #   klass = Object.const_get("HomeController")
-      #   act = "index"
-      #   controller = klass.new(env)
-      #   begin
-      #     text = controller.send(act) # assumes return is of string type
-      #     return [200, { "Content-Type" => "text/html" }, [text]]
-      #   rescue
-      #     text = "controller action not found #{act}"
-      #     return [404, { "Content-Type" => "text/html" }, [text]]
-      #   end
-      # end
 
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
